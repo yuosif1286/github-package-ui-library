b4c53e0bc6f4fc2ab52a0592ed012c029bb49826
yuosif1286 <84059356+yuosif1286@users.noreply.github.com>
2025-02-04 17:25:52 +0300

    rename file name

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "GithubPackagesUiLibrary",
-      fileName: (format) => `github-packages-ui-library.${format}.js`,
+      fileName: (format) => `halal-ui-library.${format}.js`,
     },
 
     rollupOptions: {
